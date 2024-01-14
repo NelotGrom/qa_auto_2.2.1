ca89b2298a890bef01b74fd74957dcdb19042756
Nelot <nelotgrom@gmail.com>
2024-01-14 17:47:40 +0100

    fix: few fixes and comments (HW 2.2.1)

--- calculations.js
+++ calculations.js
@@ -1,17 +1,20 @@
+var y = Number; // переменная y не определена, но теперь каждая функция знает, что это число 
+
 function increaseBalance(x, y) {
   x = x + y;
   return x;
 }
 
-function decreaseBalance(x) {
+function decreaseBalance(x, y) {
 x = x - y;
 return x;
 }
 
-function divideBalanceByAccounts(x, y) {
+function divideBalanceByAccounts(x, y) { // не хватало аргумента y
   x = x / y;
   return x;
-  console.log("x");
+  // console.log(x);
+  // return выходит из тела функции, думаю этот вызов не нужен;
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +24,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2)); // был лишний третий аргумент
 console.log(getRestAfterDivision(7000, 3));
