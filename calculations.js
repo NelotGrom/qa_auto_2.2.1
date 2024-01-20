b17a8ace47782367848d3cf9066f0887cf09356f
Nelot <nelotgrom@gmail.com>
2024-01-20 10:24:57 +0100

    fix: assignment operators changed  (HW 2.2.2)

--- calculations.js
+++ calculations.js
@@ -1,24 +1,25 @@
-var y = Number; // переменная y не определена, но теперь каждая функция знает, что это число 
+var y = Number; // переменная y не определена, но теперь каждая функция знает, что это число
 
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y; // сложение
   return x;
 }
 
-function decreaseBalance(x, y) { // не хватало аргумента y
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  // не хватало аргумента y
+  x -= y; // вычитание
+  return x;
 }
 
-function divideBalanceByAccounts(x, y) { 
-  x = x / y;
+function divideBalanceByAccounts(x, y) {
+  x /= y; // деление
   return x;
   // console.log(x);
   // return выходит из тела функции, думаю этот вызов не нужен;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y; // остаток после деления
   return x;
 }
 
